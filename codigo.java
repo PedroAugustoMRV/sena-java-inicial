eccf8a9d6ef5294b6a82e62af32a6edd8806db3b
gustavomgs <53432968+gustavomgs@users.noreply.github.com>
2023-04-29 08:17:00 -0300

    Create codigo.java

---
+++ codigo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class CalculoIdade {
+	public static void main(String args[]){
+		
+    String variavel;
+    
+    Scanner lerTeclado = new Scanner(System.in);
+		
+    variavel = lerTeclado.nextInt();
+
+		System.out.print(variavel);
+		
+	}
+
+}
